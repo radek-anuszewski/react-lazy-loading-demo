ad12c57443876275aa8222046d3c17224c776b30
Radek Anuszewski <radekqwerty@gmail.com>
2021-03-02 16:38:33 +0100

    Updates

---
+++ static/js/3.01f5dd3c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-lazy-loading-demo"]=this["webpackJsonpreact-lazy-loading-demo"]||[]).push([[3],{13:function(a,t,e){a.exports={lazy:"Lazy_lazy__YpsA9"}},14:function(a,t,e){"use strict";e.r(t);var n=e(11),s=e.n(n),c=e(13),r=e.n(c),l=e(0);t.default=function(){return Object(l.jsxs)("p",{className:r.a.lazy,children:["Text with styles, current date: ",s()(new Date).format()]})}}}]);
+//# sourceMappingURL=3.01f5dd3c.chunk.js.map
